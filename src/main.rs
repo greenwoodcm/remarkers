9c5149b50911612cd571669d7bcf6fe49ae88114
Christopher Greenwood <greenwd@amazon.com>
2023-04-17 15:39:14 -0700

    add initial CLI
    add CLI with subcommands for `sync` to sync the reMarkable directory to
    a local directory, and `convert` to convert the locally backed up
    directory (in reMarkable file format) to PDFs.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
-use tracing::metadata::LevelFilter;
-use tracing_subscriber::EnvFilter;
+use clap::{Parser, Subcommand};
+use tracing::info;
+use tracing_subscriber::{filter::Directive, EnvFilter};
 
-use std::env;
-use std::path::Path;
+use std::path::PathBuf;
 
 mod fs;
 mod model;
@@ -11,25 +11,62 @@ mod parser;
 mod render;
 mod sync;
 
+#[derive(Parser, Debug)]
+#[command()]
+struct Cli {
+    #[arg(short, long)]
+    log_level: Directive,
+
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Sync {
+        #[arg(short, long)]
+        dest_dir: PathBuf,
+    },
+    Convert {
+        #[arg(short, long)]
+        source_dir: PathBuf,
+        #[arg(short, long)]
+        output_dir: Option<PathBuf>,
+    },
+}
+
 fn main() -> Result<()> {
+    let cli = Cli::parse();
+
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::builder()
-                .with_default_directive(LevelFilter::WARN.into())
+                .with_default_directive(cli.log_level.clone())
                 .from_env_lossy(),
         )
         .init();
 
-    let args: Vec<_> = env::args().collect();
-    let root = args.get(1).expect("must provide root as first argument");
-    let notebooks = fs::scan(root)?;
+    info!("Parsed CLI command: {:?}", cli);
 
-    for notebook in notebooks.notebooks {
-        let output_path = Path::new(".")
-            .join("output")
-            .join(format!("{}.pdf", &notebook.name));
-        let parsed_notebook = parser::parse_notebook(notebook)?;
-        render::render_pdf(parsed_notebook, output_path);
+    match cli.command {
+        Command::Sync { dest_dir } => {
+            sync::sync_remarkable_to_dir(dest_dir)?;
+        }
+        Command::Convert {
+            source_dir,
+            output_dir,
+        } => {
+            let notebooks = fs::scan(source_dir)?;
+
+            let output_dir = output_dir.unwrap_or(PathBuf::from(".").join("output"));
+            info!("writing output to directory: {:?}", &output_dir);
+
+            for notebook in notebooks.notebooks {
+                let output_path = output_dir.join(format!("{}.pdf", &notebook.name));
+                let parsed_notebook = parser::parse_notebook(notebook)?;
+                render::render_pdf(parsed_notebook, output_path);
+            }
+        }
     }
 
     Ok(())
